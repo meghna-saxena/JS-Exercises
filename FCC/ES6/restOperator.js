feec2fed28b6522be9ac259781349629185264de
Meghna Srivastava <meghna.s1906@gmail.com>
2018-08-29 18:52:09 +0200

    es6 new features

---
+++ FCC/ES6/restOperator.js
@@ -0,0 +1,17 @@
+// With the rest operator, you can create functions that take a variable number of arguments. These arguments are stored in an array that can be accessed later from inside the function.
+
+function howMany(...args) {
+  return "You have passed " + args.length + " arguments.";
+}
+console.log(howMany(0, 1, 2)); // You have passed 3 arguments
+console.log(howMany("string", null, [1, 2, 3], {})); // You have passed 4 arguments.
+
+// The rest operator eliminates the need to check the args array and allows us to apply map(), filter() and reduce() on the parameters array.
+
+const sum = (function() {
+  "use strict";
+  return function sum(...args) {
+    return args.reduce((a, b) => a + b, 0);
+  };
+})();
+console.log(sum(1, 2, 3));
